b70fba13981a3ad0f945df696e0770e7c579ce83
jakesmith-101 <jakesmith@binaryrage.com>
2025-07-03 14:00:58 +0100

    fix routing issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,9 +7,15 @@
         <h1 class="mb-1 font-medium">Hello world!</h1>
         <ul class="flex gap-3 text-sm leading-normal">
             <li>
-                <a href="{{ route('login') }}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
-                    Login
-                </a>
+                @if (Route::has('login'))
+                    <a href="{{ route('login') }}" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                        Login
+                    </a>
+                @else
+                    <a href="https://laravel.com/docs/12.x/documentation" target="_blank" class="inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-normal">
+                        Learn
+                    </a>
+                @endif
             </li>
         </ul>
     </div>
